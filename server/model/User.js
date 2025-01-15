527c4379d8dd67052c5919d5969c6ec8f09d0ac5
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-16 01:39:03 +0300

    Update User.js

--- server/model/User.js
+++ server/model/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const joi = require('joi');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema({
   username:{
@@ -26,9 +28,36 @@ const UserSchema = mongoose.Schema({
     required:true,
     minlength:8
   },
-  profileImage:{
-    
+  age:Number,
+  bio:String,
+  imageProfile: {
+    type: Object,
+    default: {
+      url: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+      publicId: null
+    },
+  isAdmin: {
+    type: Boolean,
+    default: false
   }
-});
+},{timestamps:true});
 
-module.exports = mongoose.model("User",UserSchema);
+UserSchema.methods.generateAuthToken= function (){
+  jwt.sign({id:this._id,isAdmin:this.isAdmen},"SECRET");
+}
+
+const UserModel = mongoose.model("User",UserSchema);
+
+const validateRegisterUser = (obj)=>{
+  const schema = joi.object({
+    username:joi.string().trim().max(100).min(3).required(),
+    email:joi.string().trim().max(100).min(3).required().email(),
+    password:joi.string().trim().max(100).min(8).required(),
+    firstname:joi.string().max(100).min(3).required(),
+    lastname:joi.string().max(100).min(3).required(),
+    age:joi.string().trim().max(100).min(3).required(),
+  })
+}
+module.exports{
+  UserModel,
+}
