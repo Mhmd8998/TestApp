98fd1afabd0d5786b8f2715cdea6bf6431cb1274
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-16 05:01:25 +0300

    Update app.js

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const Auth = require("./router/Auth");
+const Users = require("./router/Users");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://alnasr9988:A0994660508a@cluster0.xmfbu.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0")
+.then(res => console.log("connected db"));
+
+app.use("api/auth",Auth);
+
+
+app.listen(3000,()=>{
+  console.log("server is runing on port 3000");
+});
