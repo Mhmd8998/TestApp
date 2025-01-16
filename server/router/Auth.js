1784b0a2182011945df9166e7865acb385b494aa
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-16 21:54:42 +0300

    Update Auth.js

--- server/router/Auth.js
+++ server/router/Auth.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const controller = require("../controller/Auth");
 
-router.post("register",controller.createUser);
-router.post("/login",controller.login);
+// المسار المصحح لنقطة النهاية للتسجيل
+router.post("/register", controller.createUser);
+router.post("/login", controller.login);
 
-module.exports =router;
+module.exports = router;
