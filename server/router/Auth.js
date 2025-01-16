af601661fd4f3368e1555d6ff19f77b6e5a2a2e2
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-16 03:55:09 +0300

    Update Auth.js

--- server/router/Auth.js
+++ server/router/Auth.js
@@ -1,1 +1,7 @@
+const router = require("express").Router();
+const controller = require("../controller/Auth");
 
+router.post("/Register",controller.createUser);
+router.post("/Login",controller.login);
+
+module.exports =router;
