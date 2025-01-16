f3ac63c2feb277bd9f4bd752805870c1311bd308
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-16 23:00:34 +0300

    Update page.jsx

--- client/my-app/src/app/login/page.jsx
+++ client/my-app/src/app/login/page.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'
 import axios from 'axios';
-import styles from './Register.model.css';
+import styles from './Login.model.css';
 
 const Login = () => {
   const [formData, setFormData] = useState({
