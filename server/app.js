b93b92013cca8c31d0a969232c8a034c01584c40
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-16 14:14:27 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const Auth = require("./router/Auth");
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://alnasr9988:A0994660508a@cluster0.xmfbu.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://3b006998:ZYCMhfqBQkx1EY0I@cluster0.nsqut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(res => console.log("connected db"));
 
 app.use("api/auth",Auth);
