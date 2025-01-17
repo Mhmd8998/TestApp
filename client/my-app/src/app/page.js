e1ce76ce6e39d8352961e3cde974f21522b4745c
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-17 03:09:29 +0300

    Update page.js

--- client/my-app/src/app/page.js
+++ client/my-app/src/app/page.js
@@ -1,11 +1,16 @@
+"use client"
 import Image from "next/image";
+import {useCookies} from 'react-cookie';
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [cookies] =useCookies(["access_token"]);
+  const token = cookies.access_token
   return (
     <div className={styles.page}>
     <main className={styles.main}>
     <h1>Hello World</h1>
+    <p>{token}</p>
     </main>
     </div>
   );
