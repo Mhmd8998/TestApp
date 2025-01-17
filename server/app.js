ce8e682d1e33dcf02505b58415ad5c5910cf4fb5
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-17 21:56:54 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const Auth = require("./router/Auth");
+const Users = require("./router/User");
 
 app.use(express.json());
 app.use(cors());
 
 // Add the leading "/" to the API path
 app.use("/api/auth", Auth);
+app.use("/api", Users);
 
 mongoose.connect("mongodb+srv://3b006998:ZYCMhfqBQkx1EY0I@cluster0.nsqut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(res => console.log("connected db"))
