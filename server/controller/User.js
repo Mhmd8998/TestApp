9a594156823b8245736ebbfdd35215f764ea268c
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-17 22:31:45 +0300

    Update User.js

--- server/controller/User.js
+++ server/controller/User.js
@@ -4,7 +4,7 @@ const UserModel = require('../model/User');
 module.exports={
   getAllUser:asyncHandler(async (req,res) =>{
     try{
-      const users = await UserModel.findAll();
+      const users = await UserModel.find();
       res.staus(200).json(users);
     } catch (err){
       res.status(401).json({message:err.message});
