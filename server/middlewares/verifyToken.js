bcc905874d5c14b27df33139613994307895ceb4
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-17 22:20:05 +0300

    Update verifyToken.js

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,42 +1,42 @@
 const jwt = require('jsonwebtoken');
 
-function verifyToken (req,res,next){
+function verifyToken (req, res, next){
   const authToken = req.headers.authorization;
   if (authToken){
     const token = authToken.split(" ")[1];
     try{
-      const decodepayload = jwt.verify(token,"SECRET");
-      req.user= decodepayload;
-      next()
+      const decodepayload = jwt.verify(token, "SECRET");
+      req.user = decodepayload;
+      next();
     }catch(error){
       return res.status(401).json({ message: "Invalid token" });
     }
   }else{
-    return res.status(401).json({ message: "No Token Provider" });
+    return res.status(401).json({ message: "No Token Provided" });
   }
 }
 
-function verifyTokenAndAdmin(req,res,next){
-  verifyToken(req,res,()=>{
-    if(req.user.isAdmin){
+function verifyTokenAndAdmin(req, res, next){
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin){
       next();
     }else{
-      return res.status(401).json({ message: "No allowed ,only admin" });
+      return res.status(401).json({ message: "Not allowed, only admin" });
     }
-  })
+  });
 }
 
-function verifyTokenAndUserId(req,res,next){
-  verifyToken(req,res,()=>{
-    if(req.user.id === req.params.id){
+function verifyTokenAndUserId(req, res, next){
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id){
       next();
     }else{
-      return res.status(401).json({ message: "No allowed ,only himself" });
+      return res.status(401).json({ message: "Not allowed, only the user himself" });
     }
-  })
+  });
 }
 
-module.exports{
+module.exports = {
   verifyTokenAndUserId,
   verifyTokenAndAdmin
-}
+};
