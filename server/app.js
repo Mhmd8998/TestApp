da373f0c310ee60525affd7f44380b78bee262fd
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-18 19:54:24 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const Auth = require("./router/Auth");
 const Users = require("./router/User");
 
-
-const PORT = process.env.PORT
+const DB_URL =process.env.DB_URL
+const PORT = process.env.PORT||5000
 
 
 app.use(express.json());
@@ -17,7 +17,7 @@ app.use(cors());
 app.use("/api/auth", Auth);
 app.use("/api", Users);
 
-mongoose.connect("mongoose.connect("mongodb+srv://3b006998:ZYCMhfqBQkx1EY0I@cluster0.nsqut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(DB_URL)
   .then(res => console.log("connected db"))
   .catch(err => console.error("Error connecting to DB:", err));
 
