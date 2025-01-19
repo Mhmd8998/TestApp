7958eaad4d8c73b77bce459838a25aa08dd21dab
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-19 13:42:10 +0300

    Update User.js

--- server/router/User.js
+++ server/router/User.js
@@ -3,5 +3,5 @@ const {getAllUser, updateUser}=require('../controller/User');
 const {verifyTokenAndAdmin,verifyTokenAndUserId}=require('../middlewares/verifyToken');
 
 router.route('/users').get(verifyTokenAndAdmin,getAllUser)
-router.route('/update').post(verifyTokenAndUserId,updateUser)
+router.route('/update/:id').post(verifyTokenAndUserId,updateUser)
 module.exports = router
