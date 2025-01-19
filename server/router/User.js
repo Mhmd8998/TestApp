1dd43ff2ca3cacbf04fcf618b2d728185a8bd4d5
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-19 22:34:36 +0300

    Update User.js

--- server/router/User.js
+++ server/router/User.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
-const {getAllUser, updateUser}=require('../controller/User');
-const {verifyTokenAndAdmin,verifyTokenAndUserId}=require('../middlewares/verifyToken');
+const { getAllUser, updateUser } = require('../controller/User');
+const { verifyTokenAndAdmin, verifyTokenAndUserId } = require('../middlewares/verifyToken');
 
-router.route('/users').get(verifyTokenAndAdmin,getAllUser)
-router.route('/update/:id').put(verifyTokenAndUserId,updateUser)
-module.exports = router
+// Route to get all users (only accessible to admins)
+router.route('/users').get(verifyTokenAndAdmin, getAllUser);
+
+// Route to update a user by ID (accessible to the user themselves or an admin)
+router.route('/update/:id').put(verifyTokenAndUserId, updateUser);
+
+module.exports = router;
