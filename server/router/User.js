a0359557e8c9473a1c616ec31072a9f50e088a21
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-20 17:02:19 +0300

    Update User.js

--- server/router/User.js
+++ server/router/User.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const validateObjectId = require("../middlewares/ValidateObjectId");
-const { getAllUser, updateUser } = require('../controller/User');
+const { getAllUser, updateUser,getUser } = require('../controller/User');
 const { verifyTokenAndAdmin, verifyTokenAndUserId } = require('../middlewares/verifyToken');
 
 // Route to get all users (only accessible to admins)
