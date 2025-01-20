cb7bcc7af10265c84e7cb43bca38fe1a080d9133
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-21 01:44:41 +0300

    Update Navbar.jsx

--- client/my-app/src/Components/Navbar.jsx
+++ client/my-app/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import styles from './navbar.module.css';  // سيتم استيراد التنسيقات من ملف CSS خارجي
 
 const Navbar = () => {
