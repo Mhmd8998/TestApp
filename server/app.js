6d67349a1be396675244fec252e16f30cc89ef17
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-20 21:06:16 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,13 +5,14 @@ const app = express();
 const cors = require("cors");
 const Auth = require("./router/Auth");
 const Users = require("./router/User");
-
+const bodyParser = require('body-parser');
 const DB_URL =process.env.DB_URL
 const PORT = process.env.PORT||5000
 
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json()); 
 
 // Add the leading "/" to the API path
 app.use("/api/auth", Auth);
