86e51de4e518e6215a2dca62db5be7ec3f9beac1
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-22 02:18:22 +0300

    Update User.js

--- server/router/User.js
+++ server/router/User.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const validateObjectId = require("../middlewares/ValidateObjectId");
 const { getAllUser, updateUser,getUser ,uploadProfile} = require('../controller/User');
 const { verifyTokenAndAdmin, verifyTokenAndUserId } = require('../middlewares/verifyToken');
-const UploadPhoto = require("../middlewares/UploadPhoto");
+const upload = require("../middlewares/UploadPhoto");
 
 // Route to get all users (only accessible to admins)
 router.route('/users').get(verifyTokenAndAdmin, getAllUser);
@@ -10,6 +10,6 @@ router.route('/user/:id').get(validateObjectId,verifyTokenAndUserId, getUser);
 
 // Route to update a user by ID (accessible to the user themselves or an admin)
 router.route('/update/:id').put(validateObjectId,verifyTokenAndUserId, updateUser);
-router.route("/profile/upload-peofile-photo").post(verifyTokenAndAdmin,UploadPhoto.single("image"),uploadProfile)
+router.route("/profile/upload-peofile-photo").post(verifyTokenAndUserId,upload.single('profilePic'),uploadProfile)
 
 module.exports = router;
