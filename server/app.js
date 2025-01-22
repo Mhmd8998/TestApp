5f00ae097808f0261f584b9ebb109cad09e8ce77
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-23 02:07:52 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,6 +10,16 @@ const DB_URL =process.env.DB_URL
 const PORT = process.env.PORT||5000
 
 
+
+const uploadsDir = path.join(__dirname, 'images');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
+// جعل مجلد "uploads" متاحًا للوصول عبر HTTP
+app.use('/images', express.static(uploadsDir)); // <-- تقديم الملفات من مجلد "uploads"
+
+
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json()); 
