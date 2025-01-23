570ba74573c2949a92be52c4f0feebc74c414199
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-24 02:29:15 +0300

    Update Post.js

--- server/src/controller/Post.js
+++ server/src/controller/Post.js
@@ -33,7 +33,7 @@ module.exports= {
         return res.status(404).json({ message: "Post not found" });
     }
     //تحقق من ان المستخدم نفسه صاحب المنشور
-    if(req.user.id !== post.userid.toString()){
+    if(req.user.id !== post.userId.toString()){
       return res.status(401).json({ message: "you are not allow !" });
     }
     // تحديث بيانات المنشور
