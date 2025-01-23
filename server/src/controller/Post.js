9734213a55c1118fd1ba5316a85e6012efe2887a
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-23 21:00:20 +0300

    Update Post.js

--- server/src/controller/Post.js
+++ server/src/controller/Post.js
@@ -30,5 +30,37 @@ module.exports= {
     return res.status(500).json({ message: error.message });
   }
 }),
+  updatePost: asyncHandler(async (req, res) => {
+    // التحقق من ادخال البيانات
+    const { error } = validateUpdatePost(req.body);
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+    }
+    
+    try {
+        // تحديث بيانات المنشور
+        const post = await PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    title: req.body.title,
+                    description: req.body.description,
+                }
+            },
+            { new: true }
+        );
+
+        // التحقق من وجود المنشور بعد التحديث
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        // رسالة اتمام العملية بنجاح
+        return res.status(200).json({ message: "Updated Post Successfully", post });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+})
+                                    
 
 };
