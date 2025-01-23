8cfd6f7852444576394291ee49d817ff53246b1c
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-24 00:50:08 +0300

    Update Post.js

--- server/src/router/Post.js
+++ server/src/router/Post.js
@@ -3,8 +3,7 @@ const { verifyToken } = require('../middlewares/verifyToken');
 const {createPost,updatePost} = require("../controller/Post");
 const validateObjectId = require("../middlewares/ValidateObjectId");
 
-router.route("/post")
-  .post(verifyToken,createPost);
-  .put(validateObjectId,verifyToken,updatePost);
+router.route("/post").post(verifyToken,createPost);
+router.route("/post:id").put(validateObjectId,verifyToken,updatePost);
 
 module.exports = router;
