832a13c3e81c716d1897e6264c26ba00ebbb2070
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-24 04:28:14 +0300

    Update layout.js

--- client/my-app/src/app/layout.js
+++ client/my-app/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 const geistSans = Geist({
