eaed8b19eded8a9ba0d6c7aa9b03900334eea82b
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-24 05:44:57 +0300

    Update Footer.jsx

--- client/my-app/src/Components/Footer.jsx
+++ client/my-app/src/Components/Footer.jsx
@@ -1,20 +1,13 @@
 // src/components/Footer.jsx
 import React from 'react';
-import styles from './Footer.module.css'; // استخدام CSS Modules
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <p className={styles.footerText}>© 2025 جميع الحقوق محفوظة</p>
-        <ul className={styles.footerLinks}>
-          <li><a href="/about" className={styles.footerLink}>عن الموقع</a></li>
-          <li><a href="/contact" className={styles.footerLink}>اتصل بنا</a></li>
-          <li><a href="/privacy" className={styles.footerLink}>سياسة الخصوصية</a></li>
-        </ul>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+    <footer className="bg-light text-center text-lg-start">
+      <div className="container p-4">
+        <p className="text-center">© 2025 جميع الحقوق محفوظة</p>
+        <ul className="list-unstyled d-flex justify-content-center">
+          <li className="mx-3">
+            <a href="/about" className="text-dark">عن الموقع</a>
+          </li>
+          <li className="mx-3
