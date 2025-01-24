decc04d0943d466bc4f6932b4961f76fa5e35f91
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-25 01:58:35 +0300

    Update page.jsx

--- client/my-app/src/app/post/allPost/page.jsx
+++ client/my-app/src/app/post/allPost/page.jsx
@@ -49,11 +49,11 @@ export default function Home() {
         {/* عرض المنشورات إذا كانت البيانات موجودة */}
         {posts.length > 0 ? (
           posts.map((post) => (
-            <div class="card border-dark mb-3" style="max-width: 18rem;" key={post._id}>
-               <div class="card-header">{post.userId.username} <span>{post.createdAt}</span></div>
-               <div class="card-body">
-                 <h5 class="card-title">{post.title}</h5>
-                 <p class="card-text">{post.description}</p>
+            <div className="card border-dark mb-3" style="max-width: 18rem;" key={post._id}>
+               <div className="card-header">{post.userId.username} <span>{post.createdAt}</span></div>
+               <div className="card-body">
+                 <h5 className="card-title">{post.title}</h5>
+                 <p className="card-text">{post.description}</p>
                 </div>
     
             </div>
