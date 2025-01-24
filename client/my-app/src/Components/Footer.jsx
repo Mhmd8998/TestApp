5abcbe752b35c8c1c6dd11eb6829c28611f8664c
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-24 05:49:57 +0300

    Update Footer.jsx

--- client/my-app/src/Components/Footer.jsx
+++ client/my-app/src/Components/Footer.jsx
@@ -10,18 +10,18 @@ const Footer = () => {
         <p className="text-center">© 2025 جميع الحقوق محفوظة</p>
         <ul className="list-unstyled d-flex justify-content-center">
           <li className="mx-3">
-            <Link href="/about" passHref>
-              <a className="text-dark">عن الموقع</a>
+            <Link href="/about" className="text-dark">
+              عن الموقع
             </Link>
           </li>
           <li className="mx-3">
-            <Link href="/contact" passHref>
-              <a className="text-dark">اتصل بنا</a>
+            <Link href="/contact" className="text-dark">
+              اتصل بنا
             </Link>
           </li>
           <li className="mx-3">
-            <Link href="/privacy" passHref>
-              <a className="text-dark">سياسة الخصوصية</a>
+            <Link href="/privacy" className="text-dark">
+              سياسة الخصوصية
             </Link>
           </li>
         </ul>
