4d6913f5796700487784b77b6597d5fc17926e25
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-27 14:41:06 +0300

    Update Comment.js

--- server/src/router/Comment.js
+++ server/src/router/Comment.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const validateObjectId = require("../middlewares/ValidateObjectId");
 const { verifyToken } = require('../middlewares/verifyToken');
-const {postComment}=require("../controller/Comment");
+const {postComment,getComment}=require("../controller/Comment");
 
 router.route("/comment/:id").post(validateObjectId,verifyToken,postComment);
+router.route("/post/comments/:id").get(validateObjectId,verifyToken,getComment);
 
 module.exports = router;
