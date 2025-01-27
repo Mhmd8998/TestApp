c1e305c9a01589a8faa920b8686f929d50a09b98
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-27 12:45:46 +0300

    Create Comment.js

---
+++ server/src/controller/Comment.js
@@ -0,0 +1,7 @@
+/*const asyncHandler= require("express-async-handler");
+const {CommentModel,validateAddComment} = require("../model/Comment");
+module.exports ={
+  postComment:asyncHandler(async (req,res) =>{
+    
+  })
+}*/
