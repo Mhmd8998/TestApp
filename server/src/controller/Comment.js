576094890de7dcdb4d31c1147868d4c6d2d78025
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-27 13:32:20 +0300

    Update Comment.js

--- server/src/controller/Comment.js
+++ server/src/controller/Comment.js
@@ -1,7 +1,21 @@
-/*const asyncHandler= require("express-async-handler");
+const asyncHandler= require("express-async-handler");
 const {CommentModel,validateAddComment} = require("../model/Comment");
+const {PostModel}=require("../model/Post");
 module.exports ={
   postComment:asyncHandler(async (req,res) =>{
-    
+    const {error} = validateAddComment(req.body);
+    if (error){
+      res.status(400).json({message:error.details[0].message});
+    }
+    const post = await PostModel.findById(req.params);
+    if(!post){
+      res.status(404).json({message:"Post Not Found !"});
+    }
+    const comment = await CommentModel.create({
+      postId:req.params,
+      userId:req.user.id,
+      comment:req.body.comment
+    })
+    res.status(200).json(comment);
   })
-}*/
+}
