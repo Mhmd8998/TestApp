0509c586422a2e1042d6a7862cf38a415517e6a1
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-27 15:49:59 +0300

    Update Comment.js

--- server/src/controller/Comment.js
+++ server/src/controller/Comment.js
@@ -1,22 +1,23 @@
 const asyncHandler= require("express-async-handler");
 const {CommentModel,validateAddComment} = require("../model/Comment");
 const {PostModel}=require("../model/Post");
+
 module.exports ={
   postComment:asyncHandler(async (req,res) =>{
     const {error} = validateAddComment(req.body);
     if (error){
-      res.status(400).json({message:error.details[0].message});
+      return res.status(400).json({message:error.details[0].message});
     }
     const post = await PostModel.findById(req.params.id);
     if(!post){
-      res.status(404).json({message:"Post Not Found !"});
+      return res.status(404).json({message:"Post Not Found !"});
     }
     const comment = await CommentModel.create({
       postId:req.params.id,
       userId:req.user.id,
       comment:req.body.comment
-    }),
-    res.status(200).json(comment);
+    });
+    return res.status(200).json(comment);
   }),
   getComment:asyncHandler(async (req,res) => {
     const comments = await CommentModel.find({postId:req.params.id});
